b4cec35fb2f10bcd1406d159806899adcbdb68e9
Your Name <youremail@example.com>
2025-08-10 12:53:16 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function App() {
+
+  let[todolist,setTodolist]=useState([])
+
+  let addToDoList=(event)=>{
+    let toDoName=event.target.toDoName.value;
+    // alert(toDoName);
+    if(!todolist.includes(toDoName)){
+      let finalTododlist=[...todolist,toDoName]
+      setTodolist(finalTododlist)
+    }
+
+    else{
+      toast.error("To Do Name already exists!");
+
+    }
+    event.preventDefault();
+  }
+
+  let list=todolist.map((value,index)=>{
+    return(
+      < TodolistItems value={value} key={index} indexNumber={index} todolist={todolist} setTodolist={setTodolist}/>
+    )
+  })
+
+  return (
+    <div className='main'>
+      <h1>ToDo APP</h1>
+      <form className='title-div mt-5' onSubmit={addToDoList}> 
+        <input className='todo-input' type='text' placeholder='Enter Title' name='toDoName'/> 
+        <button className='purple-btn' type='submit'>Add</button>
+      </form>
+      
+      <div className='outerDiv'>
+        <ul>
+          {/* <li>HTML <span> <i className="bi bi-trash3"></i></span></li>  */}
+          {/* < TodolistItems/> */}
+          {list}
+        </ul>
+      </div>
+
+      <ToastContainer />
+    </div>
+         
+    
+  );
+}
+
+export default App;
+
+function TodolistItems({value, indexNumber, todolist, setTodolist }){
+
+  let[status,setStatus]=useState(false)
+  let deleteRow=()=>{
+    // alert(indexNumber)
+    let finalData=todolist.filter((v,i)=>i!=indexNumber)
+    setTodolist(finalData);
+  }
+
+  let checkStatus=()=>{
+    setStatus(!status)
+  }
+
+  return(
+      <li className={(status)? 'completeTodo' : ''} onClick={checkStatus}>{value} <span onClick={deleteRow}> <i className="bi bi-trash3"></i></span></li> 
+   
+  )
+}
